de2f714dab948a59fdb1a4640b12ef36999c50b8
Felipe Steffan <felip.developer@hotmail.com>
2022-08-23 20:29:34 -0300

    Finalzando Object toString

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,8 +20,23 @@ public class Program {
         System.out.print("Quantity in stock : " );
         product.quantity = sc.nextInt();
 
-        System.out.println(product);
+        System.out.println();
+        System.out.print("Product data: " + product);
 
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock:");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.print("Updated data : " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.print("Updated data : " + product);
 
         sc.close();
     }
